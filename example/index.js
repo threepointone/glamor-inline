8e06d4b9e448dee1af845fa69e5d8d2cd3e5599b
Sunil Pai <threepointone@gmail.com>
2017-03-12 18:34:44 +0530

    format example

--- example/index.js
+++ example/index.js
@@ -2,12 +2,17 @@
 import { css, styleSheet } from 'glamor'
 import inline from '../src'
 import React from 'react'
-import { renderToString, renderToStaticMarkup } from 'react-dom/server'
-import { renderStatic } from 'glamor-server'
-const App = () => <div className={css({ color: 'red' })} {...css({ border: '1px solid green'}) }>
-  <span>Some text here</span> 
-  <span className={`${css({ color: 'blue'})} something`} {...css({ color: 'red' })}> more text </span>
-</div>
+import { renderToStaticMarkup } from 'react-dom/server'
+
+const App = () => 
+  <div className={css({ color: 'red' })} 
+    {...css({ border: '1px solid green'}) }>
+      <span>Some text here</span> 
+      <span className={`${css({ color: 'blue'})} something`} 
+        {...css({ color: 'red' })}> 
+          more text 
+      </span>
+  </div>
 
 
 
